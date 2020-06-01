ee8901c79d26240f314cb1149dd5dddc11aa2e51
dingxiuan <dingxiuan@163.com>
2020-06-01 23:53:18 +0800

    添加第二章

---
+++ src/jvmgo/ch02/README.md
@@ -0,0 +1,8 @@
+
+打开命令，上下文切换到：`../src` 目录下
+执行命令：`go install jvmgo\ch01\cmd.go`
+
+到 gopath\bin 下执行 cmd.exe 程序
+
+输入：cmd.exe -help，会打印 “Usage: cmd.exe [-options] class [args...]”
+
---
+++ src/jvmgo/ch02/cmd.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"github.com/dingxiuan/jvm-go/src/jvmgo/ch02/classpath"
+	"os"
+	"strings"
+)
+
+func main() {
+	cmd := parseCmd()
+	if cmd.versionFlag {
+		fmt.Println("version: 0.0.1")
+	} else if cmd.helpFlag {
+		printUsage()
+	} else {
+		startJVM(cmd)
+	}
+}
+
+type Cmd struct {
+	helpFlag    bool
+	versionFlag bool
+	cpOption    string
+	XjreOption  string
+	class       string
+	args        []string
+}
+
+func parseCmd() *Cmd {
+	cmd := &Cmd{}
+
+	flag.Usage = printUsage
+	flag.BoolVar(&cmd.helpFlag, "help", false, "print help message")
+	flag.BoolVar(&cmd.helpFlag, "?", false, "print help message")
+	flag.BoolVar(&cmd.versionFlag, "version", false, "print version and exit")
+	flag.StringVar(&cmd.cpOption, "classpath", "", "classpath")
+	flag.StringVar(&cmd.cpOption, "cp", "", "classpath")
+	flag.StringVar(&cmd.XjreOption, "Xjre", "", "path to jre")
+	flag.Parse()
+
+	args := flag.Args()
+	if len(args) > 0 {
+		cmd.class = args[0]
+		cmd.args = args[1:]
+	}
+	return cmd
+}
+
+func printUsage() {
+	fmt.Printf("Usage: %s [-options] class [args...]\n", os.Args[0])
+}
+
+func startJVM(cmd *Cmd) {
+	cp := classpath.Parse(cmd.XjreOption, cmd.cpOption)
+	fmt.Printf("classpath: %v class: %v args: %v\n", cp, cmd.class, cmd.args)
+
+	className := strings.Replace(cmd.class, ".", "/", -1)
+	classData, _, err := cp.ReadClass(className)
+	if err != nil {
+		fmt.Printf("Cound not find or load main class %s\n", cmd.class)
+		return
+	}
+
+	fmt.Printf("class data: %v\n", classData)
+
+}
